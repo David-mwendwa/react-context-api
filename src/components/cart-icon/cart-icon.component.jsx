7aff8a1cef6364318cd929602e6127fb30815e21
David Mwendwa <klatzdavie19@gmail.com>
2021-05-17 06:24:29 +0300

    Created CartProvider componenent, imported logic from cart-utils, wrapped the entire app with it and updated the respective components affected

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
-import CartContext from '../../contexts/cart/cart.context';
+import { CartContext } from '../../providers/cart/cart.provider';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -11,7 +11,7 @@ import './cart-icon.styles.scss';
 
 const CartIcon = ({ itemCount }) => {
   const { toggleHidden } = useContext(CartContext);
-  
+
   return (
     <div className="cart-icon" onClick={toggleHidden}>
       <ShoppingIcon className="shopping-icon" />
@@ -19,7 +19,6 @@ const CartIcon = ({ itemCount }) => {
     </div>
   );
 };
-
 
 const mapStateToProps = createStructuredSelector({
   itemCount: selectCartItemsCount,
